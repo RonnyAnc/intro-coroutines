58d7c4179b4a0b54a2a3c8e2137c157a3f37e41c
Ronny Ancorini Rodríguez <ronnyancorini@gmail.com>
2022-01-15 12:06:38 +0100

    [COMPLETE] tutorial

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -4,5 +4,19 @@ import contributors.*
 import kotlinx.coroutines.*
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org) // Executes request and blocks the current thread
+        .also { logRepos(req, it) }
+        .body() ?: listOf()
+
+    val deferreds: List<Deferred<List<User>>> = repos.map { repo ->
+        async {
+            log("starting loading for ${repo.name}")
+            service
+                .getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+        }
+    }
+    deferreds.awaitAll().flatten().aggregate()
 }
--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -7,5 +7,18 @@ suspend fun loadContributorsProgress(
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org) // Executes request and blocks the current thread
+        .also { logRepos(req, it) }
+        .body() ?: listOf()
+
+    var users = emptyList<User>()
+    for ((index, repo) in repos.withIndex()) {
+        val newUsers = service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+        users = newUsers + users
+        updateResults(users, repos.lastIndex == index)
+    }
 }
